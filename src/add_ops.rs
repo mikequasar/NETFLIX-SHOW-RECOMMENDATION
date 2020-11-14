8a66caa5316e732ee072c1e49cf76b6b57fad3ab
mikequasar <113967756+mikequasar@users.noreply.github.com>
2020-11-14 13:58:09 +0000

    Updated `README.md`

--- src/add_ops.rs
+++ src/add_ops.rs
@@ -3,4 +3,16 @@
 use crate::{
     add,
     arch::word::Word,
-    buffer::Buffer
+    buffer::Buffer,
+    helper_macros,
+    ibig::IBig,
+    primitive::{PrimitiveSigned, PrimitiveUnsigned},
+    sign::Sign::*,
+    ubig::{Repr::*, UBig},
+};
+use core::{
+    mem,
+    ops::{Add, AddAssign, Sub, SubAssign},
+};
+
+impl Add<UBig> for 
