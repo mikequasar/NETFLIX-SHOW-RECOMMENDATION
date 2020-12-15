4cf6252327d4a7b3fcb80f309264c1f285452936
mikequasar <113967756+mikequasar@users.noreply.github.com>
2020-12-15 22:51:00 +0000

    add demo

--- src/add_ops.rs
+++ src/add_ops.rs
@@ -139,4 +139,7 @@ impl Sub<&UBig> for &UBig {
 
     #[inline]
     fn sub(self, rhs: &UBig) -> UBig {
-    
+        match (self.repr(), rhs.repr()) {
+            (Small(word0), Small(word1)) => UBig::sub_word(*word0, *word1),
+            (Small(_), Large(_)) => UBig::panic_negative(),
+            (Large(buffer0), Small(word1)) => UBig::sub_large_
