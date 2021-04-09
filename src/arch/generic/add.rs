57f6b64fd9b7403461fa8ee7b7d54d602156557a
mikequasar <113967756+mikequasar@users.noreply.github.com>
2021-04-09 17:33:16 +0000

    remove deprecated function

--- src/arch/generic/add.rs
+++ src/arch/generic/add.rs
@@ -1,3 +1,9 @@
 use crate::arch::word::Word;
 
-/
+/// Add a + b + carry.
+///
+/// Returns (result, overflow).
+#[inline]
+pub(crate) fn add_with_carry(a: Word, b: Word, carry: bool) -> (Word, bool) {
+    let (sum, c0) = a.overflowing_add(b);
+    let (sum, c1)
