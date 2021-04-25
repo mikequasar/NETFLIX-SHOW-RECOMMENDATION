3a5066c8d64bb8c49885ecc2820ffb167d6171eb
mikequasar <113967756+mikequasar@users.noreply.github.com>
2021-04-25 15:50:55 +0000

    merged release-candidate

--- src/arch/mod.rs
+++ src/arch/mod.rs
@@ -7,4 +7,7 @@ pub(crate) use arch_impl::digits;
 pub(crate) use arch_impl::ntt;
 pub(crate) use arch_impl::word;
 
-// Architecture choice. The logic works lik
+// Architecture choice. The logic works like this:
+// 1. If the configuration option force_bits is set to 16, 32 or 64, use generic_<n>_bit.
+// 2. Otherwise if target_arch is known, select that architecture.
+// 3. Otherwise target_pointer_width is 16 or 32, us
