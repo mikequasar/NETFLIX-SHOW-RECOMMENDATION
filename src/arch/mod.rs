4d700bdc2ac33fa4e11a4763b848413501e10918
mikequasar <113967756+mikequasar@users.noreply.github.com>
2021-04-25 06:48:05 +0000

    Example tweak

--- src/arch/mod.rs
+++ src/arch/mod.rs
@@ -1,1 +1,10 @@
-//! Architecture dependent functionali
+//! Architecture dependent functionality.
+
+use cfg_if::cfg_if;
+
+pub(crate) use arch_impl::add;
+pub(crate) use arch_impl::digits;
+pub(crate) use arch_impl::ntt;
+pub(crate) use arch_impl::word;
+
+// Architecture choice. The logic works lik
