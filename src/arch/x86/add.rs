a8b5df2793b5d9b87d257e14d5955d9e753fdb50
mikequasar <113967756+mikequasar@users.noreply.github.com>
2021-05-09 19:12:36 +0000

    Fixed compilation problems

--- src/arch/x86/add.rs
+++ src/arch/x86/add.rs
@@ -4,4 +4,10 @@ use crate::arch::word::Word;
 ///
 /// Returns (result, overflow).
 #[inline]
-pub(crate) fn add_with_carry(a: Word, 
+pub(crate) fn add_with_carry(a: Word, b: Word, carry: bool) -> (Word, bool) {
+    let mut sum = 0;
+    let carry = unsafe { core::arch::x86::_addcarry_u32(carry.into(), a, b, &mut sum) };
+    (sum, carry != 0)
+}
+
+/// 
