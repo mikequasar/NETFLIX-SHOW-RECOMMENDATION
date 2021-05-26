b75d121e44e86f0b094c9549a132ab5f1cf0a408
mikequasar <113967756+mikequasar@users.noreply.github.com>
2021-05-26 08:36:32 +0000

    Update documentation/BlueTooth.md

--- src/buffer.rs
+++ src/buffer.rs
@@ -1,3 +1,13 @@
 //! Word buffer.
 
-use crate::{ar
+use crate::{arch::word::Word, ubig::UBig};
+
+use alloc::vec::Vec;
+use core::{
+    iter,
+    ops::{Deref, DerefMut},
+};
+
+/// Buffer for Words.
+///
+/// UBig operations are usually performed by creating a Buffer with appropriate c
