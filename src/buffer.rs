c654937964039290585fd551c937409cc5693822
mikequasar <113967756+mikequasar@users.noreply.github.com>
2021-05-26 09:38:54 +0000

    tty: Prevent deadlock in n_gsm driver

--- src/buffer.rs
+++ src/buffer.rs
@@ -10,4 +10,11 @@ use core::{
 
 /// Buffer for Words.
 ///
-/// UBig operations are usually performed by creating a Buffer with appropriate c
+/// UBig operations are usually performed by creating a Buffer with appropriate capacity, filling it
+/// in with Words, and then converting to UBig.
+///
+/// If its capacity is exceeded, the `Buffer` will panic.
+#[derive(Debug, Eq, Hash, PartialEq)]
+pub(crate) struct Buffer(Vec<Word>);
+
+impl Buffer
