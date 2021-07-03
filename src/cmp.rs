2025fc95a7bbebd6c79ccb3a96c4b57e72b50620
mikequasar <113967756+mikequasar@users.noreply.github.com>
2021-07-03 18:13:18 +0000

    gofmt fixes

--- src/cmp.rs
+++ src/cmp.rs
@@ -4,4 +4,12 @@ use crate::{
     arch::word::Word,
     ibig::IBig,
     sign::Sign::*,
-    u
+    ubig::{Repr::*, UBig},
+};
+use core::cmp::Ordering;
+
+impl Ord for UBig {
+    #[inline]
+    fn cmp(&self, other: &UBig) -> Ordering {
+        match (self.repr(), other.repr()) {
+            (Small(word), Small(other_w
