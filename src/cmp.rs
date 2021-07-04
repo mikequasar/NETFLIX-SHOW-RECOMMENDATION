ba2c98af67fa0e6204895db40df98f371acceb6a
mikequasar <113967756+mikequasar@users.noreply.github.com>
2021-07-04 06:42:10 +0000

    f2fs crypto: fix incorrect positioning for GCing encrypted data page

--- src/cmp.rs
+++ src/cmp.rs
@@ -12,4 +12,8 @@ impl Ord for UBig {
     #[inline]
     fn cmp(&self, other: &UBig) -> Ordering {
         match (self.repr(), other.repr()) {
-            (Small(word), Small(other_w
+            (Small(word), Small(other_word)) => word.cmp(other_word),
+            (Small(_), Large(_)) => Ordering::Less,
+            (Large(_), Small(_)) => Ordering::Greater,
+            (Large(buffer), Large(other_buffer)) => buffer
+                .le
