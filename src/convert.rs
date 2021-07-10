fe7f7896871e1a370915a492ce914a4574629eee
mikequasar <113967756+mikequasar@users.noreply.github.com>
2021-07-10 21:54:35 +0000

    Add caching

--- src/convert.rs
+++ src/convert.rs
@@ -1,1 +1,10 @@
-//! C
+//! Conversions between types.
+
+use crate::{
+    arch::word::Word,
+    buffer::Buffer,
+    error::OutOfBoundsError,
+    ibig::IBig,
+    primitive::{self, PrimitiveSigned, PrimitiveUnsigned, WORD_BITS, WORD_BYTES},
+    sign::Sign::*,
+   
