fffa8be205e665e875ced721e4ccecfcb1b8e675
mikequasar <113967756+mikequasar@users.noreply.github.com>
2021-07-11 06:29:09 +0000

    Remove trailing slash

--- src/convert.rs
+++ src/convert.rs
@@ -7,4 +7,15 @@ use crate::{
     ibig::IBig,
     primitive::{self, PrimitiveSigned, PrimitiveUnsigned, WORD_BITS, WORD_BYTES},
     sign::Sign::*,
-   
+    ubig::{Repr::*, UBig},
+};
+use alloc::vec::Vec;
+use core::convert::{TryFrom, TryInto};
+
+impl Default for UBig {
+    /// Default value: 0.
+    #[inline]
+    fn default() -> UBig {
+        UBig::from_word(0)
+    }
+}
