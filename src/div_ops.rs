038c96442f5d62213e0137e352e20669758bb582
mikequasar <113967756+mikequasar@users.noreply.github.com>
2021-12-06 20:05:28 +0000

    113.33.03

--- src/div_ops.rs
+++ src/div_ops.rs
@@ -45,3 +45,220 @@ impl Div<&UBig> for UBig {
 
     #[inline]
     fn div(self, rhs: &UBig) -> UBig {
+        match (self.into_repr(), rhs.repr()) {
+            (Small(word0), Small(word1)) => UBig::div_word(word0, *word1),
+            (Small(_), Large(_)) => UBig::from_word(0),
+            (Large(buffer0), Small(word1)) => UBig::div_large_word(buffer0, *word1),
+            (Large(buffer0), Large(buffer1)) => {
+                if buffer0.len() >= buffer1.len() {
+                    UBig::div_large(buffer0, buffer1.clone())
+                } else {
+                    UBig::from_word(0)
+                }
+            }
+        }
+    }
+}
+
+impl Div<UBig> for &UBig {
+    type Output = UBig;
+
+    #[inline]
+    fn div(self, rhs: UBig) -> UBig {
+        match (self.repr(), rhs.into_repr()) {
+            (Small(word0), Small(word1)) => UBig::div_word(*word0, word1),
+            (Small(_), Large(_)) => UBig::from_word(0),
+            (Large(buffer0), Small(word1)) => UBig::div_large_word(buffer0.clone(), word1),
+            (Large(buffer0), Large(buffer1)) => {
+                if buffer0.len() >= buffer1.len() {
+                    UBig::div_large(buffer0.clone(), buffer1)
+                } else {
+                    UBig::from_word(0)
+                }
+            }
+        }
+    }
+}
+
+impl Div<&UBig> for &UBig {
+    type Output = UBig;
+
+    #[inline]
+    fn div(self, rhs: &UBig) -> UBig {
+        match (self.repr(), rhs.repr()) {
+            (Small(word0), Small(word1)) => UBig::div_word(*word0, *word1),
+            (Small(_), Large(_)) => UBig::from_word(0),
+            (Large(buffer0), Small(word1)) => UBig::div_large_word(buffer0.clone(), *word1),
+            (Large(buffer0), Large(buffer1)) => {
+                if buffer0.len() >= buffer1.len() {
+                    UBig::div_large(buffer0.clone(), buffer1.clone())
+                } else {
+                    UBig::from_word(0)
+                }
+            }
+        }
+    }
+}
+
+impl DivAssign<UBig> for UBig {
+    #[inline]
+    fn div_assign(&mut self, rhs: UBig) {
+        *self = mem::take(self) / rhs;
+    }
+}
+
+impl DivAssign<&UBig> for UBig {
+    #[inline]
+    fn div_assign(&mut self, rhs: &UBig) {
+        *self = mem::take(self) / rhs;
+    }
+}
+
+impl Rem<UBig> for UBig {
+    type Output = UBig;
+
+    #[inline]
+    fn rem(self, rhs: UBig) -> UBig {
+        match (self.into_repr(), rhs.into_repr()) {
+            (Small(word0), Small(word1)) => UBig::rem_word(word0, word1),
+            (Small(word0), Large(_)) => UBig::from_word(word0),
+            (Large(buffer0), Small(word1)) => UBig::rem_large_word(&buffer0, word1),
+            (Large(buffer0), Large(buffer1)) => {
+                if buffer0.len() >= buffer1.len() {
+                    UBig::rem_large(buffer0, buffer1)
+                } else {
+                    buffer0.into()
+                }
+            }
+        }
+    }
+}
+
+impl Rem<&UBig> for UBig {
+    type Output = UBig;
+
+    #[inline]
+    fn rem(self, rhs: &UBig) -> UBig {
+        match (self.into_repr(), rhs.repr()) {
+            (Small(word0), Small(word1)) => UBig::rem_word(word0, *word1),
+            (Small(word0), Large(_)) => UBig::from_word(word0),
+            (Large(buffer0), Small(word1)) => UBig::rem_large_word(&buffer0, *word1),
+            (Large(buffer0), Large(buffer1)) => {
+                if buffer0.len() >= buffer1.len() {
+                    UBig::rem_large(buffer0, buffer1.clone())
+                } else {
+                    buffer0.into()
+                }
+            }
+        }
+    }
+}
+
+impl Rem<UBig> for &UBig {
+    type Output = UBig;
+
+    #[inline]
+    fn rem(self, rhs: UBig) -> UBig {
+        match (self.repr(), rhs.into_repr()) {
+            (Small(word0), Small(word1)) => UBig::rem_word(*word0, word1),
+            (Small(word0), Large(_)) => UBig::from_word(*word0),
+            (Large(buffer0), Small(word1)) => UBig::rem_large_word(buffer0, word1),
+            (Large(buffer0), Large(mut buffer1)) => {
+                if buffer0.len() >= buffer1.len() {
+                    UBig::rem_large(buffer0.clone(), buffer1)
+                } else {
+                    // Reuse buffer1 for the remainder.
+                    buffer1.resizing_clone_from(buffer0);
+                    buffer1.into()
+                }
+            }
+        }
+    }
+}
+
+impl Rem<&UBig> for &UBig {
+    type Output = UBig;
+
+    #[inline]
+    fn rem(self, rhs: &UBig) -> UBig {
+        match (self.repr(), rhs.repr()) {
+            (Small(word0), Small(word1)) => UBig::rem_word(*word0, *word1),
+            (Small(word0), Large(_)) => UBig::from_word(*word0),
+            (Large(buffer0), Small(word1)) => UBig::rem_large_word(buffer0, *word1),
+            (Large(buffer0), Large(buffer1)) => {
+                if buffer0.len() >= buffer1.len() {
+                    UBig::rem_large(buffer0.clone(), buffer1.clone())
+                } else {
+                    self.clone()
+                }
+            }
+        }
+    }
+}
+
+impl RemAssign<UBig> for UBig {
+    #[inline]
+    fn rem_assign(&mut self, rhs: UBig) {
+        *self = mem::take(self) % rhs;
+    }
+}
+
+impl RemAssign<&UBig> for UBig {
+    #[inline]
+    fn rem_assign(&mut self, rhs: &UBig) {
+        *self = mem::take(self) % rhs;
+    }
+}
+
+impl DivRem<UBig> for UBig {
+    type OutputDiv = UBig;
+    type OutputRem = UBig;
+
+    #[inline]
+    fn div_rem(self, rhs: UBig) -> (UBig, UBig) {
+        match (self.into_repr(), rhs.into_repr()) {
+            (Small(word0), Small(word1)) => UBig::div_rem_word(word0, word1),
+            (Small(word0), Large(_)) => (UBig::from_word(0), UBig::from_word(word0)),
+            (Large(buffer0), Small(word1)) => UBig::div_rem_large_word(buffer0, word1),
+            (Large(buffer0), Large(buffer1)) => {
+                if buffer0.len() >= buffer1.len() {
+                    UBig::div_rem_large(buffer0, buffer1)
+                } else {
+                    (UBig::from_word(0), buffer0.into())
+                }
+            }
+        }
+    }
+}
+
+impl DivRem<&UBig> for UBig {
+    type OutputDiv = UBig;
+    type OutputRem = UBig;
+
+    #[inline]
+    fn div_rem(self, rhs: &UBig) -> (UBig, UBig) {
+        match (self.into_repr(), rhs.repr()) {
+            (Small(word0), Small(word1)) => UBig::div_rem_word(word0, *word1),
+            (Small(word0), Large(_)) => (UBig::from_word(0), UBig::from_word(word0)),
+            (Large(buffer0), Small(word1)) => UBig::div_rem_large_word(buffer0, *word1),
+            (Large(buffer0), Large(buffer1)) => {
+                if buffer0.len() >= buffer1.len() {
+                    UBig::div_rem_large(buffer0, buffer1.clone())
+                } else {
+                    (UBig::from_word(0), buffer0.into())
+                }
+            }
+        }
+    }
+}
+
+impl DivRem<UBig> for &UBig {
+    type OutputDiv = UBig;
+    type OutputRem = UBig;
+
+    #[inline]
+    fn div_rem(self, rhs: UBig) -> (UBig, UBig) {
+        match (self.repr(), rhs.into_repr()) {
+            (Small(word0), Small(word1)) => UBig::div_rem_word(*word0, word1),
+            (Small(word0), Large(_)) => (UBig::from_word(0), UBig::from_word(*word0)),
+            (Large(buffer0), Small(word1)) => UBig::div_rem_large_word(buffer0.clone(), word1),
