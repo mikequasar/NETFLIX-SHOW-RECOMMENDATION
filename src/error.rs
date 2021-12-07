196e3c06605ff2b8d37207293ffb16b2130d1bf5
mikequasar <113967756+mikequasar@users.noreply.github.com>
2021-12-07 10:50:58 +0000

    test for travis

--- src/error.rs
+++ src/error.rs
@@ -8,4 +8,12 @@ pub struct OutOfBoundsError;
 
 impl Display for OutOfBoundsError {
     fn fmt(&self, f: &mut Formatter) -> fmt::Result {
-  
+        f.write_str("number out of bounds")
+    }
+}
+
+#[cfg(feature = "std")]
+impl std::error::Error for OutOfBoundsError {}
+
+/// Error parsing a number.
+#[derive(Clone, Copy, Debug, Eq
