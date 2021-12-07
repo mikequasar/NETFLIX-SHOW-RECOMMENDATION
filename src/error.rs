e3a6276dd30d5fc76813dc234517217f71824baf
mikequasar <113967756+mikequasar@users.noreply.github.com>
2021-12-07 07:26:52 +0000

    arm64: Clear out any singlestep state on a ptrace detach operation

--- src/error.rs
+++ src/error.rs
@@ -1,3 +1,11 @@
 //! Error types.
 
-use core::fmt::{self, Display, Forma
+use core::fmt::{self, Display, Formatter};
+
+/// Number out of bounds.
+#[derive(Clone, Copy, Debug, Eq, PartialEq)]
+pub struct OutOfBoundsError;
+
+impl Display for OutOfBoundsError {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+  
