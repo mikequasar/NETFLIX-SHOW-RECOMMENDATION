95934f2468df146ced1aed0b09cbdd52d2d9c810
mikequasar <113967756+mikequasar@users.noreply.github.com>
2021-12-09 18:39:05 +0000

    update zh-Hant translation

--- src/fast_divide.rs
+++ src/fast_divide.rs
@@ -3,4 +3,12 @@
 use crate::{
     arch::word::{DoubleWord, Word},
     assert::{assert_in_const_fn, debug_assert_in_const_fn},
- 
+    math,
+    primitive::{double_word, extend_word, split_double_word},
+};
+
+/// Divide a Word by a prearranged divisor.
+///
+/// Granlund, Montgomerry "Division by Invariant Integers using Multiplication"
+/// Algorithm 4.1.
+#[de
