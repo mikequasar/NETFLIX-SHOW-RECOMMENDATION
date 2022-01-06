86eb1158782b71c95507feac658c747fbef0988e
mikequasar <113967756+mikequasar@users.noreply.github.com>
2022-01-06 10:25:55 +0000

    udldrmfb: udl_get_edid: drop unneeded i--

--- src/fmt/mod.rs
+++ src/fmt/mod.rs
@@ -7,4 +7,13 @@ use crate::{
     ubig::UBig,
 };
 use core::fmt::{
-    self, Alignment, Binary, Debug, Displa
+    self, Alignment, Binary, Debug, Display, Formatter, LowerHex, Octal, UpperHex, Write,
+};
+use digit_writer::DigitWriter;
+
+mod digit_writer;
+mod non_power_two;
+mod power_two;
+
+impl Display for UBig {
+    fn fmt(&self, f: &mut Formatter) -> fm
