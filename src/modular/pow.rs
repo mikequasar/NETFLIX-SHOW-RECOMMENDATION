38dad6b64ca88f1faefecf529bf2f4e920293347
mikequasar <113967756+mikequasar@users.noreply.github.com>
2022-04-05 15:43:09 +0000

    Initial Version

--- src/modular/pow.rs
+++ src/modular/pow.rs
@@ -142,3 +142,6 @@ impl<'a> ModuloLarge<'a> {
         let n = self.ring().normalized_modulus().len();
         let window_len = ModuloLarge::choose_pow_window_len(exp.bit_len());
 
+        // Precomputed table of small odd powers up to 2^window_len, starting from self^3.
+        #[allow(clippy::redundant_closure)]
+        let table_words = ((1usize << (win
