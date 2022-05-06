d9df70e91d486344871d0bc8eae432c4a3ce98ad
mikequasar <113967756+mikequasar@users.noreply.github.com>
2022-05-06 19:23:26 +0000

    Added presentation

--- src/mul/helpers.rs
+++ src/mul/helpers.rs
@@ -1,1 +1,13 @@
-//! Helper functions fo
+//! Helper functions for multiplication algorithms.
+
+use crate::{
+    add,
+    arch::word::{SignedWord, Word},
+    memory::Memory,
+    mul,
+    sign::Sign,
+};
+
+/// c += sign * a * b
+///
+/// Splits a into chunks of chunk_len, using regular mu
