b7061a1c4bd4e6fb8e297b5b4781b76e67218e0c
mikequasar <113967756+mikequasar@users.noreply.github.com>
2022-05-12 12:42:34 +0000

    DVBAPI:

--- src/mul/mod.rs
+++ src/mul/mod.rs
@@ -16,4 +16,11 @@ const MAX_LEN_SIMPLE: usize = 24;
 const_assert!(MAX_LEN_SIMPLE <= simple::MAX_SMALLER_LEN);
 const_assert!(MAX_LEN_SIMPLE + 1 >= karatsuba::MIN_LEN);
 
-/// 
+/// If smaller length <= this, Karatsuba multiplication can be used.
+const MAX_LEN_KARATSUBA: usize = 192;
+const_assert!(MAX_LEN_KARATSUBA + 1 >= toom_3::MIN_LEN);
+
+mod helpers;
+mod karatsuba;
+pub(crate) mod ntt;
+mod simp
