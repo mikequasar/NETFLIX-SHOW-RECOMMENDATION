e851c9b8eb1fa74a3a5f637353be33315dfc79dd
mikequasar <113967756+mikequasar@users.noreply.github.com>
2022-05-12 09:25:36 +0000

    Restructure project

--- src/mul/mod.rs
+++ src/mul/mod.rs
@@ -11,4 +11,9 @@ use alloc::alloc::Layout;
 use core::mem;
 use static_assertions::const_assert;
 
-/// If smaller l
+/// If smaller length <= MAX_LEN_SIMPLE, simple multiplication can be used.
+const MAX_LEN_SIMPLE: usize = 24;
+const_assert!(MAX_LEN_SIMPLE <= simple::MAX_SMALLER_LEN);
+const_assert!(MAX_LEN_SIMPLE + 1 >= karatsuba::MIN_LEN);
+
+/// 
