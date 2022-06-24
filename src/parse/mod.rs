b0313ea9ecb1151a8baa8f7fe8d1c7a0fac00477
mikequasar <113967756+mikequasar@users.noreply.github.com>
2022-06-24 07:00:29 +0000

    vfs: check if f_count is 0 or negative

--- src/parse/mod.rs
+++ src/parse/mod.rs
@@ -78,3 +78,89 @@ impl UBig {
             UBig::from_str_radix_no_sign(oct, 8)
         } else if let Some(hex) = src.strip_prefix("0x") {
             UBig::from_str_radix_no_sign(hex, 16)
+        } else {
+            UBig::from_str_radix_no_sign(src, 10)
+        }
+    }
+
+    /// Convert an unsigned string to [UBig].
+    fn from_str_radix_no_sign(mut src: &str, radix: Digit) -> Result<UBig, ParseError> {
+        debug_assert!(radix::is_radix_valid(radix));
+        if src.is_empty() {
+            return Err(ParseError::NoDigits);
+        }
+
+        while let Some(src2) = src.strip_prefix('0') {
+            src = src2;
+        }
+
+        if radix.is_power_of_two() {
+            power_two::parse(src, radix)
+        } else {
+            non_power_two::parse(src, radix)
+        }
+    }
+}
+
+impl IBig {
+    /// Convert a string in a given base to [IBig].
+    ///
+    /// The string may contain a `+` or `-` prefix.
+    /// Digits 10-35 are represented by `a-z` or `A-Z`.
+    ///
+    /// # Panics
+    ///
+    /// Panics if `radix` is not between 2 and 36 inclusive.
+    ///
+    /// # Examples
+    /// ```
+    /// # use ibig::{error::ParseError, ibig, IBig};
+    /// assert_eq!(IBig::from_str_radix("-7ab", 32)?, ibig!(-7499));
+    /// # Ok::<(), ParseError>(())
+    /// ```
+    pub fn from_str_radix(mut src: &str, radix: u32) -> Result<IBig, ParseError> {
+        radix::check_radix_valid(radix);
+        let sign;
+        match src.strip_prefix('-') {
+            Some(s) => {
+                sign = Negative;
+                src = s;
+            }
+            None => {
+                sign = Positive;
+                src = src.strip_prefix('+').unwrap_or(src);
+            }
+        }
+        let mag = UBig::from_str_radix_no_sign(src, radix)?;
+        Ok(IBig::from_sign_magnitude(sign, mag))
+    }
+
+    /// Convert a string with an optional radix prefix to [IBig].
+    ///
+    /// `src` may contain an '+' or `-` prefix after the radix prefix.
+    ///
+    /// Allowed prefixes: `0b` for binary, `0o` for octal, `0x` for hexadecimal.
+    ///
+    /// # Examples
+    /// ```
+    /// # use ibig::{error::ParseError, ibig, IBig};
+    /// assert_eq!(IBig::from_str_with_radix_prefix("+0o17")?, ibig!(0o17));
+    /// assert_eq!(IBig::from_str_with_radix_prefix("-0x1f")?, ibig!(-0x1f));
+    /// # Ok::<(), ParseError>(())
+    /// ```
+    pub fn from_str_with_radix_prefix(mut src: &str) -> Result<IBig, ParseError> {
+        let sign;
+        match src.strip_prefix('-') {
+            Some(s) => {
+                sign = Negative;
+                src = s;
+            }
+            None => {
+                sign = Positive;
+                src = src.strip_prefix('+').unwrap_or(src);
+            }
+        }
+        let mag = UBig::from_str_with_radix_prefix_no_sign(src)?;
+        Ok(IBig::from_sign_magnitude(sign, mag))
+    }
+}
