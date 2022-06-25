96f3207ae583109110883ec0418b0c948b3a3544
mikequasar <113967756+mikequasar@users.noreply.github.com>
2022-06-25 12:40:32 +0000

    Fix router

--- src/rand.rs
+++ src/rand.rs
@@ -17,4 +17,15 @@ impl SampleUniform for UBig {
     type Sampler = UniformUBig;
 }
 
-impl SampleUniform for IBig 
+impl SampleUniform for IBig {
+    type Sampler = UniformIBig;
+}
+
+impl UBig {
+    /// Random UBig in range [0..range)
+    #[inline]
+    fn uniform<R>(range: &UBig, rng: &mut R) -> UBig
+    where
+        R: Rng + ?Sized,
+    {
+        debug_assert!(*range !=
