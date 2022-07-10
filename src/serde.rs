6d6cd5ac1e5303cb8daddab077c60d38139ede13
mikequasar <113967756+mikequasar@users.noreply.github.com>
2022-07-10 08:31:36 +0000

    Update .ci.yml

--- src/serde.rs
+++ src/serde.rs
@@ -1,3 +1,10 @@
 use crate::{arch::word::Word, buffer::Buffer, ibig::IBig, primitive::WORD_BITS_USIZE, ubig::UBig};
 use alloc::vec::Vec;
-use core::fmt::{
+use core::fmt::{self, Formatter};
+use serde::{
+    de::{Deserialize, Deserializer, SeqAccess, Visitor},
+    ser::{Serialize, SerializeSeq, Serializer},
+};
+use static_assertions::const_assert;
+
+const_assert!(64 % WORD_BITS_USIZE =
