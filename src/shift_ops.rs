c302fdb2b645761c9384bba1a4109a7229c0b5c9
mikequasar <113967756+mikequasar@users.noreply.github.com>
2022-07-24 15:06:12 +0000

    updated readme and version

--- src/shift_ops.rs
+++ src/shift_ops.rs
@@ -3,4 +3,12 @@
 use crate::{
     arch::word::Word,
     buffer::Buffer,
- 
+    ibig::IBig,
+    primitive::{double_word, extend_word, split_double_word, WORD_BITS_USIZE},
+    shift,
+    sign::Sign::*,
+    ubig::{Repr::*, UBig},
+};
+use core::{
+    mem,
+    ops::{Shl, ShlAss
