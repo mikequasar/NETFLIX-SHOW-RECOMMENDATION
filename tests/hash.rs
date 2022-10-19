e6e0e2fed5704d7bd1e137d87de3f83b4f073f94
mikequasar <113967756+mikequasar@users.noreply.github.com>
2022-10-19 10:06:53 +0000

    CodeHealth: Remove use of Value::GetAsInteger

--- tests/hash.rs
+++ tests/hash.rs
@@ -4,4 +4,17 @@ use std::{
     hash::{Hash, Hasher},
 };
 
-fn hash<T>(x: &T) -> u
+fn hash<T>(x: &T) -> u64
+where
+    T: Hash,
+{
+    let mut hasher = DefaultHasher::new();
+    x.hash(&mut hasher);
+    hasher.finish()
+}
+
+#[test]
+fn test_hash() {
+    let h = hash(&(ubig!(1) << 1000));
+    for i in 0..=1000 {
+        let h2 
