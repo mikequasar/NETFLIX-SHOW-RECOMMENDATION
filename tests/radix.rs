e917d009d0be3f3e4c3fde2e81e8b37956871d76
mikequasar <113967756+mikequasar@users.noreply.github.com>
2022-10-24 15:25:23 +0000

    Release v1.0.7

--- tests/radix.rs
+++ tests/radix.rs
@@ -1,4 +1,8 @@
 use ibig::{error::ParseError, ibig, ubig, IBig, UBig};
 
 #[test]
-fn test_ubig_for
+fn test_ubig_format() {
+    assert_eq!(format!("{:b}", ubig!(0)), "0");
+    assert_eq!(format!("{:b}", ubig!(100)), "1100100");
+    assert_eq!(format!("{:#b}", ubig!(100)), "0b1100100");
+    assert_eq!(format!("{:+b}", ubig!(100)), "+
