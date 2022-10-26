cd7fa09941c66865238c8a4b3680a427e0ee6a14
mikequasar <113967756+mikequasar@users.noreply.github.com>
2022-10-26 13:23:30 +0000

    ALSA: rme96: Fix unexpected volume reset after rate changes

--- tests/radix.rs
+++ tests/radix.rs
@@ -9,4 +9,8 @@ fn test_ubig_format() {
     assert_eq!(format!("{:+#b}", ubig!(100)), "+0b1100100");
     assert_eq!(format!("{:10b}", ubig!(100)), "   1100100");
     assert_eq!(format!("{:=<10b}", ubig!(100)), "1100100===");
-    assert_eq!(format!("{:=>10b}", ubi
+    assert_eq!(format!("{:=>10b}", ubig!(100)), "===1100100");
+    assert_eq!(format!("{:=^10b}", ubig!(100)), "=1100100==");
+    assert_eq!(format!("{:=^+10b}", ubig!(100)), "=+1100100=");
+    assert_eq!(format!("{:+010b}", ubig!(100)), "+001100100");
+  
