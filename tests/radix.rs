5f2ea65e269e05008b37b2fa8e991c6e98dd5148
mikequasar <113967756+mikequasar@users.noreply.github.com>
2022-10-26 05:48:03 +0000

    Fixed readme link

--- tests/radix.rs
+++ tests/radix.rs
@@ -5,4 +5,8 @@ fn test_ubig_format() {
     assert_eq!(format!("{:b}", ubig!(0)), "0");
     assert_eq!(format!("{:b}", ubig!(100)), "1100100");
     assert_eq!(format!("{:#b}", ubig!(100)), "0b1100100");
-    assert_eq!(format!("{:+b}", ubig!(100)), "+
+    assert_eq!(format!("{:+b}", ubig!(100)), "+1100100");
+    assert_eq!(format!("{:+#b}", ubig!(100)), "+0b1100100");
+    assert_eq!(format!("{:10b}", ubig!(100)), "   1100100");
+    assert_eq!(format!("{:=<10b}", ubig!(100)), "1100100===");
+    assert_eq!(format!("{:=>10b}", ubi
