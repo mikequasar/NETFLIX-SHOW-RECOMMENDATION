a8211392387f8d994aecb1d12d01a0021d6279a7
mikequasar <113967756+mikequasar@users.noreply.github.com>
2022-11-18 19:27:05 +0000

    Fixed test

--- tests/radix.rs
+++ tests/radix.rs
@@ -122,4 +122,7 @@ fn test_ibig_format() {
     assert_eq!(format!("{:=^+10b}", ibig!(100)), "=+1100100=");
     assert_eq!(format!("{:=^+10b}", ibig!(-100)), "=-1100100=");
     assert_eq!(format!("{:+010b}", ibig!(100)), "+001100100");
-    assert_eq!(form
+    assert_eq!(format!("{:+010b}", ibig!(-100)), "-001100100");
+    assert_eq!(format!("{:+#010b}", ibig!(100)), "+0b1100100");
+    assert_eq!(format!("{:+#010b}", ibig!(-100)), "-0b1100100");
+    assert_eq!(format!("{:
