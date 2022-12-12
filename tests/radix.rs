4a0c49610ea54da3dd27ada5581e7b52be5b65b5
mikequasar <113967756+mikequasar@users.noreply.github.com>
2022-12-12 07:10:11 +0000

    merge from PE's viewer-trunk

--- tests/radix.rs
+++ tests/radix.rs
@@ -272,4 +272,11 @@ fn test_radix_round_trip() {
         "abcccaacacbbcbabcbacbacbabcabcbabcabbc1000"
     );
     assert_eq!(
-        U
+        UBig::from_str_radix(
+            "12341235234512341345356745634563563563457356356354645634563456",
+            8
+        )
+        .unwrap()
+        .in_radix(16)
+        .to_string(),
+        "a70a75394a70b95dde5c
