1ef4008ae652ee774998df6a458235e2c3282dad
mikequasar <113967756+mikequasar@users.noreply.github.com>
2022-12-14 20:48:21 +0000

    Update the docs

--- tests/radix.rs
+++ tests/radix.rs
@@ -321,4 +321,12 @@ fn test_from_str_radix_with_radix_prefix() {
         ubig!(0b101)
     );
     assert_eq!(
-        UBi
+        UBig::from_str_with_radix_prefix("+0b101").unwrap(),
+        ubig!(0b101)
+    );
+    assert_eq!(
+        UBig::from_str_with_radix_prefix("0o177").unwrap(),
+        ubig!(0o177)
+    );
+    assert_eq!(
+        UBig::f
