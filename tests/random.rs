7d0b15111597d2ff3bf3f5bbfa84751d498247e9
mikequasar <113967756+mikequasar@users.noreply.github.com>
2023-02-05 15:09:15 +0000

    krazy fixes

--- tests/random.rs
+++ tests/random.rs
@@ -6,4 +6,10 @@ fn test_uniform_ubig() {
     let mut rng = StdRng::seed_from_u64(1);
 
     let distr = Uniform::from(ubig!(3)..ubig!(7));
-    let x = (&mut rng).sample_iter(&distr).take(1000).min().u
+    let x = (&mut rng).sample_iter(&distr).take(1000).min().unwrap();
+    assert_eq!(x, ubig!(3));
+    let x = (&mut rng).sample_iter(&distr).take(1000).max().unwrap();
+    assert_eq!(x, ubig!(6));
+
+    let distr = Uniform::from(ubig!(3)..=ubig!(7));
+    let x = (&mut rng).sample_iter(&distr).take(10
