d34a4ae4d8368f62e9aa68b8eb6660f42510aee9
mikequasar <113967756+mikequasar@users.noreply.github.com>
2023-02-05 16:19:04 +0000

    Updated default config

--- tests/random.rs
+++ tests/random.rs
@@ -12,4 +12,9 @@ fn test_uniform_ubig() {
     assert_eq!(x, ubig!(6));
 
     let distr = Uniform::from(ubig!(3)..=ubig!(7));
-    let x = (&mut rng).sample_iter(&distr).take(10
+    let x = (&mut rng).sample_iter(&distr).take(1000).min().unwrap();
+    assert_eq!(x, ubig!(3));
+    let x = (&mut rng).sample_iter(&distr).take(1000).max().unwrap();
+    assert_eq!(x, ubig!(7));
+
+    let distr = Uniform::from(ubig!(0b100) << 128..ubig!(0b1000) <
