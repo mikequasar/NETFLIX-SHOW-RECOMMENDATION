3bfd6d2c7ecc442f4bbf893ee9029f6e79e2b27c
mikequasar <113967756+mikequasar@users.noreply.github.com>
2023-02-05 08:53:53 +0000

    added attribution

--- tests/random.rs
+++ tests/random.rs
@@ -1,4 +1,9 @@
 use ibig::{ibig, ops::DivRem, ubig, UBig};
 use rand::{distributions::uniform::Uniform, prelude::*};
 
-#[t
+#[test]
+fn test_uniform_ubig() {
+    let mut rng = StdRng::seed_from_u64(1);
+
+    let distr = Uniform::from(ubig!(3)..ubig!(7));
+    let x = (&mut rng).sample_iter(&distr).take(1000).min().u
